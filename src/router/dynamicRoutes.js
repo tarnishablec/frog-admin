215db4e2b688ac4e0b7f30d4ee21917577b4d48c
tarnishablec <tarnishablec@outlook.com>
2019-06-05 14:44:52 +0800

    done

--- src/router/dynamicRoutes.js
+++ src/router/dynamicRoutes.js
@@ -1,5 +1,15 @@
 const dynamicRoutes = [
-
+	{
+		path: '/test',
+		name: 'test',
+		components: {
+			default: () => import('@/views/common/Test'),
+		},
+		meta: {
+			icon: 'formatpainter-fill',
+			role: 'root',
+		}
+	},
 ];
 
 export default dynamicRoutes;
---
+++ src/views/common/Test.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		Test
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Test"
+	}
+</script>
+
+<style scoped>
+
+</style>
