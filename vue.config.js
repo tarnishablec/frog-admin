0bdf40e29c91de755fe4100a9add4a5dc0d88ed1
tarnishablec <tarnishablec@outlook.com>
2019-06-05 11:35:38 +0800

    vue config

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const resolve = dir => require('path').join(__dirname, dir);
+
+module.exports = {
+    chainWebpack: (config) => {
+		config.resolve.alias
+			.set('@', resolve('src'))
+	},
+
+    devServer: {
+		proxy: {
+			'/api1': {
+				target: 'http://172.16.10.37:8008/',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api1': ''
+				}
+			}
+		}
+	},
+    runtimeCompiler: true
+};
