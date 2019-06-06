7afb589f67892dc5d80820e4f6c1c1a084047f56
tarnishablec <tarnishablec@outlook.com>
2019-06-06 10:46:35 +0800

    element $func

--- src/plugins/element-ui.js
+++ src/plugins/element-ui.js
@@ -4,7 +4,11 @@ import {
 	Menu,
 	Submenu,
 	MenuItem,
-	Tooltip
+	Tooltip,
+	Loading,
+	MessageBox,
+	Message,
+	Notification
 } from 'element-ui'
 
 Vue.use(Button);
@@ -12,3 +16,12 @@ Vue.use(Menu);
 Vue.use(Submenu);
 Vue.use(MenuItem);
 Vue.use(Tooltip);
+Vue.use(Loading.directive);
+
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
