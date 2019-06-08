6ba292aa83e21c00dd9f1e060e42b4a489171e30
tarnishablec <tarnishablec@outlook.com>
2019-06-09 01:44:51 +0800

    github pages

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,15 @@
 const resolve = dir => require('path').join(__dirname, dir);
 
 module.exports = {
-    chainWebpack: (config) => {
+	publicPath: process.env.NODE_ENV === 'production'
+		? '/frog-admin/'
+		: '/',
+	chainWebpack: (config) => {
 		config.resolve.alias
 			.set('@', resolve('src'))
 	},
 
-    devServer: {
+	devServer: {
 		proxy: {
 			'/api1': {
 				target: 'http://172.16.10.37:8008/',
@@ -14,5 +17,5 @@ module.exports = {
 			}
 		}
 	},
-    runtimeCompiler: true
+	runtimeCompiler: true
 };
