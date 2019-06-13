f0f379f75328fde00b8afb2bc6a074fb536aaad5
tarnishablec <tarnishablec@outlook.com>
2019-06-13 22:26:36 +0800

    table curd operation and hooks

--- src/components/frog-ui/table/index.vue
+++ src/components/frog-ui/table/index.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="fr-table" v-loading="!data" :class="{'fr-table-loading':!data}">
 		<el-table class="fr-table-body" :border="border" v-if="data" ref="elTable"
-		          :data="data.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize)"
+		          :data="showData"
 		          :stripe="stripe" :fit="fit" :show-header="showHeader"
 		          :highlight-current-row="highlightCurrentRow" :max-height="maxHeight"
 		          @selection-change="handleSelectionChange">
 			<slot name="expand"/>
 			<el-table-column v-if="selection" type="selection"/>
-			<el-table-column v-if="index" type="index"/>
+			<el-table-column v-if="index" type="index" :index="indexStart"/>
 			<el-table-column v-if="fullLoad" v-for="(value,name) in data[0]" :key="name" :prop="name" :label="name"
 			                 :show-overflow-tooltip="ellipsis"
 			                 :align="align">
@@ -24,6 +24,37 @@
 					<el-input v-if="editingRow === scope.$index" v-model="scope.row[column]"/>
 				</template>
 			</el-table-column>
+			<el-table-column label="Operations" align="center" width="200" v-if="editable&&removable">
+				<template slot="header" v-if="addable">
+					<el-button size="mini" type="primary" @click="startAddingRow">new</el-button>
+				</template>
+				<template slot-scope="scope">
+					<div v-if="scope.$index===0&&isAddingRow">
+						<el-button
+								size="mini"
+								@click="$emit('rowAdd',scope.$index, scope.row)">confirm
+						</el-button>
+						<el-button
+								size="mini"
+								type="danger"
+								@click="cancelAdd">cancel
+						</el-button>
+					</div>
+					<div v-else>
+						<el-button
+								v-if="editable"
+								size="mini"
+								@click="$emit('rowChange',scope.$index, scope.row)">update
+						</el-button>
+						<el-button
+								v-if="removable"
+								size="mini"
+								type="danger"
+								@click="$emit('rowRemove',scope.$index, scope.row)">remove
+						</el-button>
+					</div>
+				</template>
+			</el-table-column>
 			<slot/>
 		</el-table>
 		<el-pagination v-if="pagination && data" :total="data.length"
@@ -35,6 +66,7 @@
 </template>
 
 <script>
+	import {deepCopy} from '@/utils/commonUtils'
 
 	export default {
 		name: "frTable",
@@ -77,16 +109,35 @@
 				default: true,
 			},
 			editable: Boolean,
+			removable: Boolean,
+			addable: Boolean,
 			columns: {
 				type: Array,
 			}
 		},
-
+		computed: {
+			showData() {
+				let sd = this.data.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize);
+				let ss = deepCopy(sd[0]);
+				for (let s in ss) {
+					ss[s] = '-----';
+				}
+				if (this.isAddingRow) {
+					sd.splice(0, 0, ss);
+				}
+				return sd;
+			},
+			indexStart() {
+				return this.isAddingRow ? 0 : 1;
+			},
+		},
 		data() {
 			return {
+				sourceData: this.data,
 				currentPage: 1,
 				selectedRows: [],
 				editingRow: null,
+				isAddingRow: false,
 			}
 		},
 		methods: {
@@ -98,6 +149,14 @@
 			},
 			enableEdit(index) {
 				this.editingRow = index;
+			},
+			startAddingRow() {
+				this.isAddingRow = true;
+				this.editingRow = 0;
+			},
+			cancelAdd() {
+				this.editingRow = null;
+				this.isAddingRow = false;
 			}
 		},
 	}
--- src/views/common/Test/Table/index.vue
+++ src/views/common/Test/Table/index.vue
@@ -1,24 +1,23 @@
 <template>
 	<div>
-		<fr-table :columns="columns" ellipsis stripe index border selection :data="data" ref="table1">
-			<el-table-column :show-overflow-tooltip="true">
-				<template slot-scope="scope">
-					{{scope.row.title}}++++++{{scope.row.id}}
-				</template>
-			</el-table-column>
+		<fr-table :columns="columns" :data="data" ref="table1"
+		          ellipsis stripe index border selection
+		          removable editable addable
+		          @rowChange="changeRow"
+		          @rowRemove="removedRow"
+		          @rowAdd="addRow">
 			<el-table-column slot="expand" type="expand">
 				<template slot-scope="scope">
 					{{scope.row.title}}
 				</template>
 			</el-table-column>
 		</fr-table>
-		<el-button @click="print" style="margin-right: 20px">Print Selected</el-button>
+		<el-button @click="printSelected" style="margin-right: 20px">Print Selected</el-button>
 		<span>=> check in console</span>
 	</div>
 </template>
 
 <script>
-
 	export default {
 		name: "testTable",
 		asyncComputed: {
@@ -26,15 +25,24 @@
 				return (await this.$axios.get('https://jsonplaceholder.typicode.com/posts')).data;
 			},
 		},
-		data(){
-			return{
-				columns:['userId','id','title']
+		data() {
+			return {
+				columns: ['userId', 'id', 'title']
 			}
 		},
 		methods: {
-			print() {
+			printSelected() {
 				console.log(this.$refs.table1.selectedRows)
 			},
+			changeRow(index, row) {
+				console.log(index, row)
+			},
+			removedRow(index, row) {
+				console.log(index, row)
+			},
+			addRow(index, row) {
+				console.log(index, row)
+			}
 		}
 	}
 </script>
