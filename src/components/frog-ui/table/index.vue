59ac31090ce2e14989cb386cc3fff3a3b472b85d
tarnishablec <tarnishablec@outlook.com>
2019-06-13 16:04:25 +0800

    fix expand

--- src/components/frog-ui/table/index.vue
+++ src/components/frog-ui/table/index.vue
@@ -5,7 +5,7 @@
 		          :stripe="stripe" :fit="fit" :show-header="showHeader"
 		          :highlight-current-row="highlightCurrentRow" :max-height="maxHeight"
 		          @selection-change="handleSelectionChange">
-			<el-table-column v-if="expand" type="expand"/>
+			<slot name="expand"/>
 			<el-table-column v-if="selection" type="selection"/>
 			<el-table-column v-if="index" type="index"/>
 			<el-table-column v-if="fullLoad" v-for="(value,name) in data[0]" :key="name" :prop="name" :label="name"
@@ -76,7 +76,6 @@
 				type: String,
 				default: 'left',
 			},
-			expand: Boolean,
 			pagination: {
 				type: Boolean,
 				default: true,
--- src/views/common/Test/Table/index.vue
+++ src/views/common/Test/Table/index.vue
@@ -1,19 +1,9 @@
 <template>
 	<div>
 		<fr-table ellipsis full-load stripe index border selection :data="data" ref="table1">
-			<el-table-column label="Operations" align="center" width="200">
+			<el-table-column slot="expand" type="expand">
 				<template slot-scope="scope">
-					<div>
-						<el-button
-								size="mini"
-								@click="handleEdit(scope.$index, scope.row)">edit
-						</el-button>
-						<el-button
-								size="mini"
-								type="danger"
-								@click="handleDelete(scope.$index, scope.row)">remove
-						</el-button>
-					</div>
+					{{scope.row.title}}
 				</template>
 			</el-table-column>
 		</fr-table>
@@ -35,34 +25,6 @@
 			print() {
 				console.log(this.$refs.table1.selectedRows)
 			},
-			handleEdit(index, row) {
-				this.$alert(row, 'edit   ' + row.id, {
-					confirmButtonText: '确定',
-					callback: action => {
-						this.$message({
-							type: 'info',
-							message: `action: ${action}`
-						});
-					}
-				})
-			},
-			handleDelete(index, row) {
-				this.$confirm('This will permanently delete the file. Continue?', 'Warning', {
-					confirmButtonText: 'OK',
-					cancelButtonText: 'Cancel',
-					type: 'warning'
-				}).then(() => {
-					this.$message({
-						type: 'success',
-						message: 'Delete completed'
-					});
-				}).catch(() => {
-					this.$message({
-						type: 'info',
-						message: 'Delete canceled'
-					});
-				});
-			}
 		}
 	}
 </script>
