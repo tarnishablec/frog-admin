0086979ea69e4689067a82d31819620d3e16e9e2
tarnishablec <tarnishablec@outlook.com>
2019-06-17 15:57:33 +0800

    some change

--- src/views/common/Playground/index.vue
+++ src/views/common/Playground/index.vue
@@ -1,26 +1,35 @@
 <template>
 	<section id="playground">
 		<h1>Playground</h1>
-		<tr v-for="(name,index) in list">
-			<td>{{name}}</td>
-			<td >
-				<button @click="list.splice(index,1)">delete</button>
-			</td>
-		</tr>
+		<table>
+			<tr v-for="(name,index) in list">
+				<td>{{name}}</td>
+				<td>
+					<button @click="deleteByIndex(index)">delete</button>
+				</td>
+			</tr>
+		</table>
 	</section>
 </template>
 
 <script>
 	export default {
 		name: "playground",
-		data(){
-			return{
-				list:['盖伦','提莫','祈求者'],
+		data() {
+			return {
+				list: ['盖伦', '提莫', '祈求者'],
+			}
+		},
+		methods: {
+			deleteByIndex(index) {
+				this.$confirm('sure?').then(() => {
+					this.list.splice(index, 1)
+				}).catch(() => {
+				})
 			}
 		}
 	}
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
 </style>
